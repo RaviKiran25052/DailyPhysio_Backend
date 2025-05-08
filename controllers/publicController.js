4674cb328eb32cc9ce669b1614c67177cf66e7a5
RaviKiran25052 <ravivarma25052@gmail.com>
2025-05-08 16:58:20 +0530

    'trending'

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -3,15 +3,20 @@ const Exercise = require('../models/Exercise');
 
 exports.getTrendingData = async (req, res) => {
     try {
-        // Get top 5 trending therapists based on consultationCount and followers
-        const trendingTherapists = await Therapist.find()
+        // Get top 5 trending active therapists
+        const trendingTherapists = await Therapist.find({ status: "active" })
             .sort({ consultationCount: -1, followers: -1 })
-            .limit(5)
+            .limit(5);
 
-        // Get top 5 trending exercises based on views and favorites
-        const trendingExercises = await Exercise.find()
+        // Get top 5 trending exercises with filters and excluding the video field
+        const trendingExercises = await Exercise.find({
+            isPremium: false,
+            'custom.createdBy': { $ne: 'proUser' },
+            'custom.type': 'public'
+        })
+            .select('-video') // deselect the video field
             .sort({ views: -1, favorites: -1 })
-            .limit(5)
+            .limit(5);
 
         res.status(200).json({
             success: true,
@@ -27,4 +32,4 @@ exports.getTrendingData = async (req, res) => {
             error: error.message
         });
     }
-}; 
+};
